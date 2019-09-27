8f36f0b07ecc6dca3e853a069f43095489d0ab14
Kwinten Olaerts <olaertskwinten@gmail.com>
2019-09-27 17:00:14 +0200

    j

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log('ayee')
+
+// // document.getElementById("h1-id").innerHTML = "New text!";
+// document.getElementById("btn").style.color = 'blue';
+
+
+
+// function changeColor(newColor){
+//     var a = document.getElementById("h1-id");
+//     a.style.innerHTML = newColor;
+//     alert("yeee");
+// }
+
